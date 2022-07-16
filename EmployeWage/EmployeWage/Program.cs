d968e477e44ff18f10feb71901183d90b8f0d3bb
Sourav2707 <smartsourav33@gmail.com>
2022-07-16 14:32:26 +0530

    [Sourav] - Already using class

--- EmployeWage/EmployeWage/Program.cs
+++ EmployeWage/EmployeWage/Program.cs
@@ -1,4 +1,5 @@
-﻿using EmployeeWage;
+﻿//Already using class since starting
+using EmployeeWage;
 Console.WriteLine("Welcome to employee wage computation program");
 Emp emp = new Emp();
 emp.Wage();
