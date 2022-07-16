4b594f8b6657176dc703b7b913edaccdabda5517
Sourav2707 <smartsourav33@gmail.com>
2022-07-16 15:00:03 +0530

    [Sourav] - checkinng for multiple companies

--- EmployeWage/EmployeWage/Program.cs
+++ EmployeWage/EmployeWage/Program.cs
@@ -1,5 +1,6 @@
 ﻿//Already using class since starting
 using EmployeeWage;
 Console.WriteLine("Welcome to employee wage computation program");
-Emp emp = new Emp();
-emp.Wage();
+IEmployeeWageForCompany details = new Wages();
+details.compute("IBM", 30, 18, 80);
+details.compute("TATA", 35, 25, 115);
